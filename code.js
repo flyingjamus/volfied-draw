3c609518391c36f7f5c4bf54cf8dbb9389dc7bab
Danny Shternberg <flyingjamus@gmail.com>
2016-01-02 03:12:15 +0200

    Add firebase

--- code.js
+++ code.js
@@ -17,9 +17,68 @@
   var prevKey;
   var svg;
 
-  var squiglyness;
-  var samplingRate;
-  var chaos;
+  var squiglyness = 3;
+  var samplingRate = 0.05;
+  var chaos = 1.5;
+
+  var db;
+
+  var CHECK = {
+    x: 0,
+    y: 90 * 4,
+    width: 40 * 4,
+    height: 40 * 4,
+    jump: 2
+  };
+
+  function getOriginalImageData() {
+    var levelImage = document.querySelector("#level-image");
+    var hiddenCanvas = document.createElement('canvas');
+    hiddenCanvas.width = WIDTH;
+    hiddenCanvas.height = HEIGHT;
+    hiddenCanvas.getContext('2d').drawImage(levelImage, 0, 0, WIDTH, HEIGHT);
+    return hiddenCanvas.getContext('2d').getImageData(0, 0, WIDTH, HEIGHT);
+  }
+
+  function sameImages(image1, image2) {
+    var i;
+    var j;
+    var currentIndex;
+    for (i = CHECK.x; i <= CHECK.x + CHECK.width; i = i + CHECK.jump) {
+      for (j = CHECK.y; j <= CHECK.y + CHECK.height; j = j + CHECK.jump) {
+        currentIndex = j * WIDTH * 4 + i;
+        if (image1[currentIndex] !== image2[currentIndex]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  function callOnSimilarImage(context, originalImageData, callback) {
+    var INTERVAL = 200;
+    var sameImage = true;
+    function resembleLoop(newTimeStamp) {
+      var diff = newTimeStamp - lastTimeStamp;
+      if (diff > INTERVAL) {
+        lastTimeStamp = newTimeStamp;
+        var imageData = context.getImageData(0, 0, WIDTH, HEIGHT);
+        if (sameImages(originalImageData.data, imageData.data)) {
+          if (!sameImage) {
+            sameImage = true;
+            callback.call();
+          }
+        } else {
+          sameImage = false;
+        }
+      }
+      requestAnimationFrame(resembleLoop);
+    }
+
+    var lastTimeStamp = performance.now();
+    requestAnimationFrame(resembleLoop);
+  }
+
 
   function setParams() {
     chaos = document.querySelector("#chaos-input").value;
@@ -31,20 +90,44 @@
     return (Math.random() - 0.5) * squiglyness;
   }
 
-  function drawSquigly(x1, y1, x2, y2, dx1, dy1, dx2, dy2) {
+  function updateData(x1, y1, x2, y2, x3, y3, x4, y4) {
+    var update = {};
+    update[dataIndex] = {
+      x1: x1,
+      y1: y1,
+      x2: x2,
+      y2: y2,
+      x3: x3,
+      y3: y3,
+      x4: x4,
+      y4: y4,
+    };
+    if (!dbImage) {
+      console.log(12221312)
+      dbImage = db.push({
+        date: Firebase.ServerValue.TIMESTAMP,
+        data: []
+      });
+    }
+    dbImage.child('data').update(update);
+  }
+
+  function makeSquigley(x1, y1, x2, y2, dx1, dy1, dx2, dy2) {
     var xDiff = (x2 - x1) / 3;
     var yDiff = (y2 - y1) / 3;
     dx1 = dx1 || randSquig();
     dy1 = dy1 || randSquig();
     dx2 = dx2 || randSquig();
     dy2 = dy2 || randSquig();
-    drawBezier(x1, y1, x1 + xDiff + dx1, y1 + yDiff + dy1, x1 + xDiff * 2 + dx2, y1 + yDiff * 2 + dy2, x2, y2);
+    updateData(x1, y1, x1 + xDiff + dx1, y1 + yDiff + dy1, x1 + xDiff * 2 + dx2, y1 + yDiff * 2 + dy2, x2, y2);
   }
 
   function drawBezier(x1, y1, x2, y2, x3, y3, x4, y4) {
     var path = 'M' + x1 + ',' + y1 + ' C' + x2 + ',' + y2 + ' ' + x3 + ',' + y3 + ' ' + x4 + ',' + y4;
     svg.path(path)
   }
+
+  var dataIndex = 0;
 
   function handleKeys() {
     var pressedKeys = [];
@@ -58,7 +141,7 @@
       if (isValidDirectionKey(pressedKey)) {
         looping = true;
         if (diff > samplingRate) {
-
+          dataIndex++;
           lastTimeStamp = currentTimeStamp;
           var prevX = x;
           var prevY = y;
@@ -102,7 +185,8 @@
             prevX = 0;
           }
 
-          drawSquigly(prevX, prevY, x, y);
+          makeSquigley(prevX, prevY, x, y);
+
           prevKey = pressedKey;
         }
         requestAnimationFrame(loop);
@@ -136,20 +220,98 @@
     return keyCode && keyCode >= 37 && keyCode <= 40;
   }
 
-  window.onload = function() {
+  var dbImage;
+  var currentImageKey;
+  var currentIndex = 0;
 
+  function updateImage(imageData) {
+    if (imageData) {
+      imageData.slice(currentIndex).forEach(function(data) {
+        drawBezier(data.x1, data.y1, data.x2, data.y2, data.x3, data.y3, data.x4, data.y4);
+      });
+      currentIndex = imageData.length;
+    }
+  }
 
-    var dosboxCanvas = document.querySelector("#dosbox-canvas");
-    var drawingSvg = document.querySelector("#drawing-svg");
+  function requestFullScreen(el) {
+    el.style.width = '100%';
+    var clientRect = el.getBoundingClientRect();
+    el.style.height = 400 * clientRect.width / 640;
+    el.style.width = clientRect.width;
+    var request = (el.requestFullScreen || el.webkitRequestFullScreen || el.mozRequestFullScreen);
+    request.call(el);
+  }
+
+  function setPerma(link) {
+    document.querySelector("#permalink").href = link;
+  }
+
+  function loadViewer(drawingSvg) {
     svg = SVG(drawingSvg).size(WIDTH, HEIGHT);
 
+    function update(imagesSnapshot) {
+      if (currentImageKey !== imagesSnapshot.key()) {
+        svg.clear();
+        currentIndex = 0;
+        currentImageKey = imagesSnapshot.key();
+        setPerma('/drawings/' + currentImageKey);
+      }
+      updateImage(imagesSnapshot.val().data);
+    }
 
+    var path = window.location.pathname.split('/');
+    if (path[path.length - 2] === 'drawings') {
+      db.orderByKey()
+        .equalTo(path[path.length - 1])
+        .limitToLast(1)
+        .on('child_changed', update)
+      db.orderByKey()
+        .equalTo(path[path.length - 1])
+        .limitToLast(1)
+        .on('child_added', update)
+    } else {
+      db.orderByChild('date')
+        .limitToLast(1)
+        .on('child_changed', update)
+      db.orderByChild('date')
+        .limitToLast(1)
+        .on('child_added', update)
+    }
+
+    var prevHeight;
+    var prevWidth;
+    var clientRect = drawingSvg.getBoundingClientRect();
+    prevHeight = clientRect.height;
+    prevWidth = clientRect.width;
+    window.viewerFullscreen = requestFullScreen.bind(null, drawingSvg)
+
+    window.addEventListener("resize", function() {
+      if (!document.isFullScreen && !document.webkitIsFullScreen && !document.mozIsFullScreen) {
+        drawingSvg.style.height = prevHeight + 'px';
+        drawingSvg.style.width = prevWidth + 'px';
+      }
+    });
+    document.querySelector("#full-screen-viewer").addEventListener('click', function(e) {
+      e.preventDefault();
+      requestFullScreen(drawingSvg);
+    });
+
+    document.querySelector("#save-as-png").addEventListener('click', function(e) {
+      e.preventDefault();
+      saveSvgAsPng(drawingSvg, 'volfied.png');
+    });
+  }
+
+  function loadGame(dosboxCanvas) {
+
+    var dosboxContext = dosboxCanvas.getContext('2d');
+
+    var originalImageData = getOriginalImageData();
     handleKeys();
-    setParams();
-    document.addEventListener('keyup', setParams);
-    document.addEventListener('focusout', setParams);
-    document.querySelector("#clear-btn").addEventListener('click', function() {
-      svg.clear();
+
+    callOnSimilarImage(dosboxContext, originalImageData, function() {
+      dbImage = null;
+      dataIndex = 0;
     });
 
     var emulator = new Emulator(dosboxCanvas,
@@ -159,6 +321,37 @@
         DosBoxLoader.mountZip("c", DosBoxLoader.fetchFile('Game File', 'libs/Volfied_1991.zip')),
         DosBoxLoader.startExe('Volfied/VOLFIED.EXE')));
     emulator.start({ waitAfterDownloading: false });
+
+    var prevHeight;
+    var prevWidth;
+    var clientRect = dosboxCanvas.getBoundingClientRect();
+    prevHeight = clientRect.height;
+    prevWidth = clientRect.width;
+
+    document.querySelector("#full-screen-dosbox").addEventListener('click', function(e) {
+      e.preventDefault();
+      requestFullScreen(dosboxCanvas);
+    });
+
+    window.addEventListener("resize", function() {
+      if (!document.isFullScreen && !document.webkitIsFullScreen && !document.mozIsFullScreen) {
+        dosboxCanvas.style.height = prevHeight + 'px';
+        dosboxCanvas.style.width = prevWidth + 'px';
+      }
+    });
+
+  }
+
+  window.onload = function() {
+    db = new Firebase("https://volfieddraw.firebaseio.com/images");
+    var drawingSvg = document.querySelector("#drawing-svg");
+    if (drawingSvg) {
+      loadViewer(drawingSvg);
+    }
+    var dosboxCanvas = document.querySelector("#dosbox-canvas");
+    if (dosboxCanvas) {
+      loadGame(dosboxCanvas);
+    }
 
   };
 })();
